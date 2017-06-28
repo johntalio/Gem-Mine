5582018d913826356c3225d7938080bf98a9e9c5
John Talio <johntalio@yahoo.com>
2017-06-28 12:15:46 -0400

    Create very basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#root'
+  get '/static_pages' => 'static_pages#favorites', as: 'favorites'
 end
