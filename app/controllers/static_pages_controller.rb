27b60d6ca85a2ecbda7a855c495dbc47def7c4bd
John Talio <johntalio@yahoo.com>
2017-06-29 13:38:58 -0400

    Logic for root action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def root
+    @mines = Mine.all
+    if params[:search]
+      @mines = Mine.search(params[:search]).order("created_at DESC")
+    else
+      flash[:notice] = "Oh no! Looks like that gem can't be found."
+    end
   end
 
   def favorites
