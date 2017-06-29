ec46880676161a790b708ca7daf8691bf66687c3
John Talio <johntalio@yahoo.com>
2017-06-29 10:52:13 -0400

    Change value to placeholder

--- app/assets/javascripts/components/SearchBar.es6.jsx
+++ app/assets/javascripts/components/SearchBar.es6.jsx
@@ -2,7 +2,7 @@ class SearchBar extends React.Component {
   render() {
     return(
       <form className="search-form-container">
-        <input className="search-form" type="text" value="Search" />
+        <input className="search-form" type="text" placeholder="Search" />
         <input type="button" id="search-button" />
       </form>
     )
