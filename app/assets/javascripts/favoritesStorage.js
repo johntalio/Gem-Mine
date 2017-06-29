ff43015bdd3ccffe41eaed793b1793ba1a3ab4df
John Talio <johntalio@yahoo.com>
2017-06-29 13:39:12 -0400

    Comment out logic for now

--- app/assets/javascripts/favoritesStorage.js
+++ app/assets/javascripts/favoritesStorage.js
@@ -1,8 +1,8 @@
-if (typeof(Storage) !== "undefined") {
-  // store gems
-  sessionStorage.setItem("name", "cucumber");
-  // retrieve gems and insert them into an html element
-  document.getElementById("bank-container").innerHTML = sessionStorage.getItem();
-} else {
-  console.log("Sorry, no web storage support for you...")
-}
+// if (typeof(Storage) !== "undefined") {
+//   // store gems
+//   sessionStorage.setItem("name", "cucumber");
+//   // retrieve gems and insert them into an html element
+//   document.getElementById("bank-container").innerHTML = sessionStorage.getItem("name");
+// } else {
+//   console.log("Sorry, no web storage support for you...")
+// }
