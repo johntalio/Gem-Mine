d364a99e1d36bd168691021cded81e8b5326443b
John Talio <johntalio@yahoo.com>
2017-06-30 13:43:29 -0400

    AJAX search working now

--- app/assets/javascripts/components/Root.es6.jsx
+++ app/assets/javascripts/components/Root.es6.jsx
@@ -2,23 +2,20 @@ class Root extends React.Component {
   constructor() {
     super()
     this.gemInfo = this.gemInfo.bind(this)
-    this.state = {gems: []}
   }
 
-  // need to enable the retrieval of rubygems using the gem
-  // the url below should be a local route that, in the controller, directs to a method in the model that uses the rubygems gem to search for a gem name
-  gemInfo(gemName) {
+  gemInfo(query) {
     $.ajax({
-      url: "/",
-      data: "name=" + gemName
+      method: 'POST',
+      url: "/search",
+      data: `query=${query}`
     }).done((response) => {
-      console.log(response);
-      this.setState({gems: response})
+      $('.success-link').remove()
+      $('.search-form-container').append(response);
     })
   }
 
   render() {
-    console.log(this.state)
     return(
       <div className="search-container">
         <header className="search-header">
