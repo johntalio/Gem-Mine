f6222f3ffb2f14d4c459772679dd8878b3815841
John Talio <johntalio@yahoo.com>
2017-06-30 13:42:35 -0400

    New route for search#create path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#root'
   get '/static_pages' => 'static_pages#favorites', as: 'favorites'
+
+  post '/search' => 'search#create'
 end
