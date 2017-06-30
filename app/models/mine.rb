dd41290d27d3e94375ee1b707893877adb80285f
John Talio <johntalio@yahoo.com>
2017-06-30 13:41:13 -0400

    Delete model - unnecessary effort

--- app/models/mine.rb
+++
@@ -1,9 +0,0 @@
-class Mine < ApplicationRecord
-  validates :name, :info, presence: true
-  validates_uniqueness_of :name
-
-  def self.query(gem_name)
-    query = Gems.info(gem_name)
-    Mine.find_or_create_by(name: query["name"], info: query["info"], dependencies: query["dependencies"].to_s, documentation_uri: query["documentation_uri"])
-  end
-end
