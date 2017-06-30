2cde3d17032143ba81b84730962bcf61b1734f23
John Talio <johntalio@yahoo.com>
2017-06-30 15:11:22 -0400

    No longer necessary

--- app/assets/javascripts/favoritesStorage.js
+++
@@ -1,8 +0,0 @@
-// if (typeof(Storage) !== "undefined") {
-//   // store gems
-//   localStorage.setItem("name", "cucumber");
-//   // retrieve gems and insert them into an html element
-//   document.getElementById("bank-container").innerHTML = localStorage.getItem("name");
-// } else {
-//   console.log("Sorry, no web storage support for you...")
-// }
